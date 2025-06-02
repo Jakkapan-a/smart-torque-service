9a4072bbaf83b1cb6326f64530493357fab79e99
Jakkapan-a <jakkapan.attala@gmail.com>
2025-06-02 11:56:20 +0700

    Add swagger document

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import path, { join } from 'path';
 import * as hbs from 'hbs';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function printRoutes(server: any) {
   // Get the routes map
   const router = server._router;
+  console.log(router);
+  // Filter and map the routes to get the path and method
   const availableRoutes = router.stack.filter((r) => r.route).map((r) =>{
     return {
       route: {
@@ -29,7 +32,7 @@ async function bootstrap() {
   app.engine('hbs', hbs.__express);
   app.setViewEngine('hbs');
   
-  const server = app.getHttpAdapter().getInstance();
+  // const server = app.getHttpAdapter().getInstance();
   hbs.registerPartials(join(__dirname, '..', 'src/views/partials'));
   app.set('view options', { layout: 'layouts/main' });
   const configService = app.get(ConfigService);
@@ -37,8 +40,18 @@ async function bootstrap() {
   console.log(configService.get('database'));
 
 
+  const config = new DocumentBuilder()
+  .setTitle("API Documentation")
+  .setDescription("API description")
+  .setVersion("1.0")
+  .addTag("api")
+  .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+  
   await app.listen(configService.get('port'));
-  printRoutes(server); 
+  // printRoutes(server);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
